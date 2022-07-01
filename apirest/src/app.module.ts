6a66a599df3fb573a83445606d0dbf5b8d77e306
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2022-06-30 23:24:02 -0300

    enviroment files and config added

--- apirest/src/app.module.ts
+++ apirest/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MatchesService } from './services/matches/matches.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MatchesService],
 })
 export class AppModule {}
