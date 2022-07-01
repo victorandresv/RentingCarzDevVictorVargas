952174e5cac7eb147768cea7071236f9c3617115
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2022-07-01 10:14:44 -0300

    front view matches added

--- matches/src/List.js
+++ matches/src/List.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class List extends Component{
 
@@ -6,7 +7,8 @@ class List extends Component{
         super(props);
 
         this.state = {
-            list: []
+            list: [],
+            competition: {}
         }
         console.log("OK");
     }
@@ -15,10 +17,17 @@ class List extends Component{
         fetch('http://localhost:4000/matches/next-dates').then(response => {
             response.json().then(res =>{
                 this.setState({
-                    list: res.matches
+                    list: res.matches,
+                    competition:{
+                        count: res.resultSet.count,
+                        first_date: res.resultSet.first,
+                        last_date: res.resultSet.last,
+                        played: res.resultSet.played,
+                        image: res.competition.emblem,
+                        cname: res.competition.name,
+                        type: res.competition.type
+                    }
                 });
-                //console.log(res.matches)
-                console.log(this.state.list)
             }).catch(err => console.log(err))
         }).catch(err => {
             console.log(err)
@@ -28,17 +37,59 @@ class List extends Component{
     render(){
         return(
             <div className='container'>
+
+                <div className='card padding-10'>
+                    <div className='row'>
+                        <div className='col'>
+                            Games: {this.state.competition.count}
+                        </div>
+                        <div className='col'>
+                            First Date: {this.state.competition.first_date}
+                        </div>
+                        <div className='col'>
+                            First Date: {this.state.competition.last_date}
+                        </div>
+                        <div className='col'>
+                            Played: {this.state.competition.played}
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col col-1'>
+                            <img className='team-logo' src={this.state.competition.image}/>
+                        </div>
+                        <div className='col'>
+                            <div className='row'>
+                                <div className='col'>
+                                    Competition: {this.state.competition.cname}
+                                </div>
+                            </div>
+                            <div className='row'>
+                                <div className='col'>
+                                    Type: {this.state.competition.type}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <table className='table table-stripped'>
                     <thead>
                         <tr>
+                            <th className='text-center'>Area</th>
+                            <th className='text-center'>Date</th>
                             <th className='text-center'>Home Team</th>
                             <th className='text-center'>Away Team</th>
-                            <th className='text-center'>Score</th>
                         </tr>
                     </thead>
                     <tbody>
                         {this.state.list.map((item) => (
                             <tr>
+                                <td className='text-center'>
+                                    <img className='team-logo' src={item.area.flag}/>
+                                    <br/>
+                                    {item.area.name}
+                                </td>
+                                <td className='text-center'>{moment(item.utcDate, 'YYYY-MM-DD hh:mm').format('YYYY-MM-DD hh:mm A')}</td>
                                 <td className='text-center'>
                                     <img className='team-logo' src={item.homeTeam.crest}/>
                                     <br/>
@@ -49,7 +100,6 @@ class List extends Component{
                                     <br/>
                                     {item.awayTeam.name}
                                 </td>
-                                <td></td>
                             </tr>
                         ))}
                     </tbody>
